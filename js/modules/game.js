f319f3cf9d097a0dfdf6af5bda4bab400a97b230
Emerald Mclaniyi <mclaniyi@systemspecs.com.ng>
2020-06-09 21:04:46 +0100

    new fix on fight

--- js/modules/game.js
+++ js/modules/game.js
@@ -7,8 +7,8 @@ export class Game {
         this.grid = new Grid(8,8, this.combatTurn.bind(this));
         let defaultWeapon =  new Weapon('baton', 10);
         this.players = [
-             new Player("player-1", defaultWeapon),
-             new Player("player-2", defaultWeapon)
+             new Player("Player1", defaultWeapon),
+             new Player("Player2", defaultWeapon)
         ];
         this.weapons = [
             new Weapon("axe", 30),
@@ -35,59 +35,56 @@ export class Game {
         //  initialize
 		this.fight(attackingPlayer, otherPlayer);
     }
-    hidePlayer1() {
-		$('#Player1-attack').hide();
-		$('#Player1-defend').hide();
-		$('#Player2-attack').show();
-		$('#Player2-defend').show();
+    hidePlayer1(attackingPlayer, otherPlayer) {
+        console.log(otherPlayer.name);
+        $('#' + attackingPlayer.name + '-attack').hide();
+		$('#' + attackingPlayer.name + '-defend').hide();
+		$('#' + otherPlayer.name + '-attack').show();
+		$('#' + otherPlayer.name + '-defend').show();
 	}
 
-	hidePlayer2() {
-		$('#Player2-attack').hide();
-		$('#Player2-defend').hide();
-		$('#Player1-attack').show();
-		$('#Player1-defend').show();
+	hidePlayer2(attackingPlayer, otherPlayer) {
+		$('#' + otherPlayer.name + '-attack').hide();
+		$('#' + otherPlayer.name + '-defend').hide();
+		$('#' + attackingPlayer.name + '-attack').show();
+		$('#' + attackingPlayer.name + '-defend').show();
     }
     
     fight = (attackingPlayer, otherPlayer) => {
         let vm = this;
 		let player1 = attackingPlayer;
 		let player2 = otherPlayer;
-		$('#Player1-attack').click(function () {
-            vm.hidePlayer1();
+		$('#' + attackingPlayer.name+ '-attack').click(function () {
+            vm.hidePlayer1(attackingPlayer, otherPlayer);
             player1.defend = false;
-            player2.lifePoints = player2.lifePoints - player1.weapon.attackPower;
-            console.log(player2.lifePoints);
+            player2.lifePoints = player2.lifePoints - (player2.defend ? (player1.weapon.attackPower/2) : player1.weapon.attackPower);
+            //console.log(player2.lifePoints);
             $('#Player2-life').text(player2.lifePoints);
-            if(player2.defend === true){
-                player2.lifePoints = player2.lifePoints - player1.weapon.attackPower/2
-                $('#Player2-life').text(player2.lifePoints);
-                player2.defend = false;
-            };
-            if(player2.lifePoints === 0){
-                vm.hidePlayer1();
-                vm.hidePlayer2();
+            
+            if(player2.lifePoints <= 0){
+                vm.hidePlayer1(attackingPlayer, otherPlayer);
+                vm.hidePlayer2(attackingPlayer, otherPlayer);
                 console.log('Player1 Wins')
             }
         });
         
         $('#Player2-defend').click(function () {
-            vm.hidePlayer2();
+            vm.hidePlayer2(attackingPlayer, otherPlayer);
             player2.defend = true;
             $('#defence-player2').text(player2.defend);
         });
         
-        $('#Player2-attack').click(function () {
-            vm.hidePlayer2();
-            player1.lifePoints = player1.lifePoints - player2.weapon.attackPower;
+        $('#' + otherPlayer.name+ '-attack').click(function () {
+            vm.hidePlayer2(attackingPlayer, otherPlayer);
+            player1.lifePoints = player1.lifePoints - (player1.defend ? (player2.weapon.attackPower/2) : player2.weapon.attackPower);
             console.log(player1.lifePoints);
             $('#Player1-life').text(player1.lifePoints);
-            if(player1.defend === true){
-                player1.lifePoints = player1.lifePoints - player2.weapon.attackPower/2
-                $('#Player1-life').text(player1.lifePoints);
-                player1.defend = false;
-            };
-            
+            player2.defend = false;
+            if(player1.lifePoints <= 0){
+                vm.hidePlayer1(attackingPlayer, otherPlayer);
+                vm.hidePlayer2(attackingPlayer, otherPlayer);
+                console.log('Player2 Wins')
+            }
         });
         
         $('#Player1-defend').click(function () {
